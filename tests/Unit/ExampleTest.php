85a35958491d50250d24819e907faf4f867b310c
Harshavardhan Singam <harshavardahn singam>
2024-09-04 16:03:27 +0530

    Install Pest into the project

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,16 +1,6 @@
 <?php
 
-namespace Tests\Unit;
-
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_example(): void
-    {
+    test('example', function () {
         $this->assertTrue(true);
-    }
-}
+    });
+

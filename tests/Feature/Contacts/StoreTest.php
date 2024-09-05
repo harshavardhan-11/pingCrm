41c44422ba15d4b3dc9b3dd6bb0f73aa4be2cfa4
Harshavardhan Singam <harshavardahn singam>
2024-09-05 10:15:43 +0530

    Refactor StoreTest for contact with datasets

--- tests/Feature/Contacts/StoreTest.php
+++ tests/Feature/Contacts/StoreTest.php
@@ -3,8 +3,8 @@
 use App\Models\Contact;
 use function Pest\Faker\fake;
 
-it('can store a contact', function () {
-    login()->post('/contacts', [
+it('can store a contact', function (array $data) {
+    login()->post('/contacts', [... [
         'first_name' => fake()->firstName,
         'last_name' => fake()->lastName,
         'email' => fake()->email,
@@ -14,7 +14,7 @@ it('can store a contact', function () {
         'region' => 'Derbyshire',
         'country' => fake()->randomElement(['us', 'ca']),
         'postal_code' => fake()->postcode,
-    ])
+    ], ...$data])
         ->assertRedirect('/contacts')
         ->assertSessionHas('success', 'Contact created.');
 
@@ -25,4 +25,9 @@ it('can store a contact', function () {
         ->phone->toBePhoneNumber()
         ->region->toBe('Derbyshire')
         ->country->toBeIn(['us', 'ca']);
-});
+})->with([
+    'deafult' => [[]],
+    'email with spaces' => [['email'=> '"Harsha vardhan"@gmail.com']],
+    "email with .co.in" => [['email' => 'harshavardhan@gmail.co.in']],
+    'postal code above 25' => [['postal_code' => str_repeat("h", 25)]]
+]);
